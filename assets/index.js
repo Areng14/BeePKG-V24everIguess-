b4a3a38f08b8f0cdbd350d007c709efd73b50894
BoxOfUnity <Ace_of_fives@hotmail.com>
2022-06-08 22:19:16 -0400

    Better Testing Found. undoing messups here.

--- assets/index.js
+++ assets/index.js
@@ -7,10 +7,7 @@ import { BeePackage } from "./BeePackage.class.js";
 function ElementSelect(x, p = document) { return p.querySelector(x) }
 
 
-/* The below is a developing assistance tool. Should always remain set to "false" */
-var devTool = false;
-/* The below is the button for turning on dev (cannot be accessed except through f12 devtools) */
-const btnDev = ElementSelect('#button-dev');
+
 
 
 /* The below is the file in question. */
@@ -31,11 +28,11 @@ function removeAllChildren(el) {
 	while (el.lastChild) { el.removeChild(el.lastChild) }
 }
 
-function setupPackage(json={}) {
+function setupPackage(json = {}) {
 	pkg = new BeePackage(json);
 
 	// Run HTML setup, append generated html to container
-	
+
 	// This isn't necessary. Packages can't be loaded by file yet.
 	// removeAllChildren(q('#pkg-container'));
 	ElementSelect('#pkg-container').appendChild(pkg.html());
@@ -44,7 +41,7 @@ function setupPackage(json={}) {
 		btnDownload.disabled = true;
 		btnDownload.innerText = 'Processing...';
 
-		pkg.export().then((x)=>{
+		pkg.export().then((x) => {
 
 			btnDownload.innerText = 'Saving...';
 			if (zipType == "zip") {
@@ -53,20 +50,19 @@ function setupPackage(json={}) {
 				btnDownload.disabled = false;
 				btnDownload.innerHTML = 'Download .zip';
 			}
-			else if (zipType == "bee")
-			{
+			else if (zipType == "bee") {
 				saveAs(x, `ucp_${pkg.idl}.bee_pack`);
 				btnDownload.disabled = false;
 				btnDownload.innerHTML = 'Download .bee';
 			};
 
-		}).catch((err)=>{
-			
+		}).catch((err) => {
+
 			btnDownload.disabled = false;
 			btnDownload.innerText = 'Download';
 
-			console.warn( 'An error occurred:\n\n', err );
-			alert( 'An error occurred:\n\n' + err + '\n\nDumped to console.' );
+			console.warn('An error occurred:\n\n', err);
+			alert('An error occurred:\n\n' + err + '\n\nDumped to console.');
 
 		})
 	}
@@ -80,7 +76,7 @@ function restoreSave() {
 			return JSON.parse(LZString.decompressFromUTF16(stored));
 	}
 	catch {
-		console.warn( 'Your package could not be recovered successfully.', stored );
+		console.warn('Your package could not be recovered successfully.', stored);
 		alert('Your package could not be recovered successfully. Save dumped to console.');
 	}
 	return {}
@@ -88,16 +84,15 @@ function restoreSave() {
 
 var needsSave = true;
 
-btnSave.onclick = function() {
+btnSave.onclick = function () {
 	if (!needsSave) { return }
 	this.classList.remove('needs-save');
 	btnSave.innerText = 'Changes Saved';
-	localStorage.setItem( 'beepkg-autosave', pkg.compress() );
+	localStorage.setItem('beepkg-autosave', pkg.compress());
 	needsSave = false;
 }
 
-btnZipTypeToggle.onclick = function ()
-{
+btnZipTypeToggle.onclick = function () {
 	if (zipType == "zip") {
 		zipType = "bee";
 		btnZipTypeToggle.innerHTML = "to .zip"
@@ -109,44 +104,25 @@ btnZipTypeToggle.onclick = function ()
 		btnDownload.innerHTML = "Download .zip"
 	};
 }
-btnMergePack.onclick = function ()
-{
+btnMergePack.onclick = function () {
 	alert("This button is in alpha developing state. No proper function yet available.")
-	if (devTool) {
-		console.warn("logged in as dev, running package.")
-		var page = document.getElementById("mainHTML");
-		page.innerHTML += `<div id="grey-screen"></div>`
-    }
-	
-}
-
-btnDev.onClick = function ()
-{
-	console.warn("Dev button clicked. isDev = "+devTool)
-	if (devTool) {
-		devTool = false;
-	}
-	else {
-		devTool = true;
-    }
-	
 }
 
 function beginAutosaveLoop() {
 
-	ElementSelect('#pkg-container').addEventListener('input',()=>{
+	ElementSelect('#pkg-container').addEventListener('input', () => {
 		btnSave.classList.add('needs-save');
 		btnSave.innerText = 'Save Now';
 		needsSave = true;
 	})
 
-	setInterval( ()=>{
+	setInterval(() => {
 		if (!needsSave) { return }
 		btnSave.classList.remove('needs-save');
 		btnSave.innerText = 'Changes Saved';
-		localStorage.setItem( 'beepkg-autosave', pkg.compress() )
+		localStorage.setItem('beepkg-autosave', pkg.compress())
 		needsSave = false;
-	}, 1000*30 )
+	}, 1000 * 30)
 }
 
 setupPackage(restoreSave())
