c737cfb551592c53019c3d755e0de6f11fd55509
BoxOfUnity <Ace_of_fives@hotmail.com>
2022-06-11 21:11:08 -0400

    oops, lets do some debug reading first  ':D I promise I can code

--- assets/index.js
+++ assets/index.js
@@ -196,7 +196,7 @@ function beginAutosaveLoop() {
 const inpdeleteable = document.querySelector(`*[data-click="add-item"]`)
 inpdeleteable.onclick = function() { this._templateClickActions[key](inpdeleteable) }*/
 //setupPackage();
-restoreSave(true);
+//restoreSave(true);
 btnRestoreSave.onclick = function () {
 	document.getElementById("might-delete1").remove();
 	document.getElementById("might-delete2").remove();
