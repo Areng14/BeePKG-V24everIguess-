3b60b614b66972f100a7fc9c4ca9c21067dc438d
BoxOfUnity <Ace_of_fives@hotmail.com>
2022-06-21 00:10:55 -0400

    style cleaning and import new thing

--- assets/index.js
+++ assets/index.js
@@ -230,7 +230,7 @@ function beginAutosaveLoop() {
 /* restore package should be encapsled by a button onClick function, but only after we get a way to add package w/out restore */
 
 setupPackage(restoreSave(true), true);
-console.log("booting up version 2.7a1.6 N/A")
+console.log("booting up version 2.7a1.6 N/A 1.2")
 btnForceSave.onclick = function () {
 	beginAutosaveLoop();
 	btnForceSave.disabled = true;
